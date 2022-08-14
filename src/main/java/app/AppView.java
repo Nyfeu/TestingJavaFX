17b45e41917ab96fb30d314425d96ec23286cde4
André Maiolini <andre.maiolini@hotmail.com>
2022-08-14 17:42:47 -0300

    Finalizando o projeto!

--- src/main/java/app/AppView.java
+++ src/main/java/app/AppView.java
@@ -1,17 +1,13 @@
 package app;
 
 import gui.util.Alerts;
-import gui.util.Constraints;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 
 public class AppView extends Application {
 
